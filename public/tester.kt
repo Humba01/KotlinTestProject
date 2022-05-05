8552891861a3c48fa20fc3eaa98b710c0d1aa698
Humberto Bravo <humbandroid@gmail.com>
2022-05-05 00:30:52 -0300

    KotlinTestProject {HN Labs} - Testando a linguagem Kotlin.

---
+++ public/tester.kt
@@ -0,0 +1,29 @@
+import kotlin.Char;
+import kotlin.Int;
+import kotlin.Float;
+import kotlin.Long;
+import kotlin.Short;
+import kotlin.Unit;
+
+// val y: Int = 9;
+
+// var t: Float = 10.0;
+
+// var u: Double = 10.0;
+
+// var x: Bool = true;
+
+// var e: Character = 'a';
+
+// var w: String = "Hello, World!";
+
+// fun main(args: Array<String>) {
+//     println(x);
+// }
+
+// class A {
+//     fun foo() {
+//         println(x);
+//     }
+// }
+
